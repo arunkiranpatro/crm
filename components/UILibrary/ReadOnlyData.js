e993c887f1db041f83e3861433fad4046ddfd518
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-06 23:52:07 +0530

    fixed

--- components/UILibrary/ReadOnlyData.js
+++ components/UILibrary/ReadOnlyData.js
@@ -2,13 +2,12 @@ import React from "react";
 import { FaCopy } from "react-icons/fa";
 
 const Details = function(props) {
-  const { label, value, ccicon = "false" ,children,className} = props;
+  const { label, value, ccicon = "false", children, className = "" } = props;
   let body;
-  if(value && value!==""){
-    body=<span className="field-value">{value}</span>;
-  }
-  else if(children){
-    body=children;
+  if (children) {
+    body = children;
+  } else if (value && value !== "") {
+    body = <span className="field-value">{value}</span>;
   }
   return (
     <div className={className}>
@@ -17,9 +16,7 @@ const Details = function(props) {
           {label}:{"   "}
         </label>
       )}
-     {
-       body
-     }
+      {body}
       {ccicon === "true" && (
         <i className="cc-icon">
           <FaCopy />
