2ea63884bab835e2e12f40f83de18e9f0d4d7c26
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-07 16:08:36 +0530

    fixd

--- components/PureComponents/CCTransactionLogRow.js
+++ components/PureComponents/CCTransactionLogRow.js
@@ -6,7 +6,7 @@ export default function CCTransactionLogRow({ data }) {
   return (
     <Layout className="widget-row" columns="4">
       <Moment format="DD-MM-YYYY hh:mm a">{data.TransAuthDate}</Moment>
-      <>{data.FIDetails.IssueNumber}</>
+      <>{"xxxx-" + data.FIDetails.IssueNumber}</>
       <>{data.ActionDetails}</>
       <>{data.FIDetails.Currency}</>
     </Layout>
