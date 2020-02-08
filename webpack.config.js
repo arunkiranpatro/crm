ef743cbe12753005878b2dd33b978731e423185f
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-08 19:56:40 +0530

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,8 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
-          //MiniCssExtractPlugin.loader,
+          //"style-loader",
+          MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
