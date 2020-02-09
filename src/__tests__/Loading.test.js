573c6dcd57cb7fb087d9decd5aa2c3d997aa17db
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-09 15:27:24 +0530

    fixed tests

--- src/__tests__/Loading.test.js
+++ src/__tests__/Loading.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 import Loading from "../components/UILibrary/Loading";
 
 describe("A Loading Test Suite", function() {
@@ -9,13 +9,13 @@ describe("A Loading Test Suite", function() {
     expect(markup.find("div").text()).toContain("Loading..");
     expect(markup.hasClass("loading-div")).toBe(true);
   });
-  it("should render with class name", function() {
+  it("should render with string", function() {
     const markup = shallow(<Loading>In Progress</Loading>);
     expect(markup.find("div")).toHaveLength(1);
     expect(markup.find("div").text()).toContain("In Progress");
     expect(markup.hasClass("loading-div")).toBe(true);
   });
-  it("should render with class name", function() {
+  it("should render with children", function() {
     const markup = shallow(
       <Loading>
         <div>test</div>
